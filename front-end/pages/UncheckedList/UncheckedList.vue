99442e05c2f556349f8d3654f46331192a0476a7
Chancy <1821768109@qq.com>
2025-04-14 10:38:54 +0800

    未签到列表基本对接完毕（请假功能还待实现）

--- front-end/pages/UncheckedList/UncheckedList.vue
+++ front-end/pages/UncheckedList/UncheckedList.vue
@@ -1,6 +1,32 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<view v-if="uncheckedList.length" class="card" v-for="(item, index) in uncheckedList" :key="index">
+			<text class="label">课程名称：</text> 
+			<text>{{ item.className }}</text> 
+			<view class="spacer"></view>
+			
+			<text class="label">开始时间：</text>
+			<text>{{ item.startTime }}</text>
+			<view class="spacer"></view>
+			
+			<text class="label">签到方式：</text>
+			<text>{{ item.method }}</text>
+			<view class="spacer"></view>
+
+			<!-- 按钮区域 -->
+			<view class="action-buttons">
+				<button type="default" size="mini" @click="onLeave(item)">请假</button>
+				<button type="primary" size="mini" @click="onSignIn(item)">签到</button>
+			</view>
+		</view>
+
+		<view v-else class="empty">
+			<text>暂无未签到课程</text>
+		</view>
+
+		<view class="btn-wrap">
+			<button type="primary" @click="Check_In">获取未签到信息</button>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +34,105 @@
 	export default {
 		data() {
 			return {
-				
+				uncheckedList: []
 			}
 		},
 		methods: {
-			
+			Check_In() {
+				wx.cloud.callContainer({
+					config: {
+						env: 'prod-7glwxii4e6eb93d8'
+					},
+					path: `/EnterClassToSelectUser/GetUncheckedList?userId=${encodeURIComponent(this.$globalData.username)}`,
+					header: {
+						'X-WX-SERVICE': 'userinfo',
+						'content-type': 'application/json'
+					},
+					method: 'GET',
+					success: (res) => {
+						console.log('后端返回数据:', res);
+						if (Array.isArray(res.data) && res.data.length > 0) {
+							this.uncheckedList = res.data;
+						} else {
+							this.uncheckedList = [];
+							uni.showToast({ title: '暂无未签到课程', icon: 'none' });
+						}
+					},
+					fail: (err) => {
+						console.error('请求失败:', err);
+						uni.showToast({ title: '网络异常，请稍后重试', icon: 'none', duration: 1000 });
+					}
+				});
+			},
+			onLeave(item) {
+				// 暂未定义
+				console.log('请假按钮点击：', item);
+			},
+			onSignIn(item) {
+				const method = item.method;
+
+				const methodToPage = {
+					GPS: '/pages/Location_Check_In/Location_Check_In',
+					CIPHER: '/pages/Cipher_Check_In/Cipher_Check_In',
+					FACE_SCAN: '/pages/Face_Check_In/Face_Check_In',
+					QRCODE: '/pages/QRCode_Check_In/QRCode_Check_In'
+				};
+
+				const targetPage = methodToPage[method];
+
+				if (targetPage) {
+					uni.navigateTo({
+						url: `${targetPage}?classId=${item.classId}&startTime=${encodeURIComponent(item.startTime)}`
+					});
+				} else {
+					uni.showToast({
+						title: '未知的签到方式',
+						icon: 'none'
+					});
+				}
+			}
 		}
 	}
 </script>
 
 <style>
+.container {
+	padding: 20rpx;
+}
 
+.card {
+	background-color: #f8f8f8;
+	padding: 20rpx;
+	margin-bottom: 20rpx;
+	border-radius: 12rpx;
+	box-shadow: 0 2rpx 8rpx rgba(0,0,0,0.1);
+}
+
+.label {
+	font-weight: bold;
+	margin-right: 10rpx;
+}
+
+.spacer {
+	height: 10rpx;
+}
+
+.btn-wrap {
+	margin-top: 40rpx;
+	text-align: center;
+}
+
+.empty {
+	text-align: center;
+	color: #999;
+	margin-top: 50rpx;
+	font-size: 28rpx;
+}
+
+.action-buttons {
+	display: flex;
+	justify-content: flex-end;
+	gap: 20rpx;
+	margin-top: 20rpx;
+}
 </style>
