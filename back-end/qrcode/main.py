fcdb3379ef00ce028d468b109f2ebd208965f601
hesarice <1217812178@qq.com>
2025-04-20 20:48:08 +0800

    qrcode适配docker

--- back-end/qrcode/main.py
+++ back-end/qrcode/main.py
@@ -105,4 +105,4 @@ def generate_qrcode():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5007)
--- back-end/query/query/src/main/java/com/scut/entities/EnterIdToSelectClass.java
+++
@@ -1,9 +0,0 @@
-package com.scut.entities;
-
-import lombok.Data;
-
-@Data
-public class EnterIdToSelectClass {
-    private String classId;
-    private int total;
-}
