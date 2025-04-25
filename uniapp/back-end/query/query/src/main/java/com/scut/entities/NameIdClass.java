a2df15871037fefd383de7a45dbaed57428685ff
闫旭明 <15588194+yan-xuming01@user.noreply.gitee.com>
2025-04-25 20:48:22 +0800

    0425，id和name映射

---
+++ uniapp/back-end/query/query/src/main/java/com/scut/entities/NameIdClass.java
@@ -0,0 +1,9 @@
+package com.scut.entities;
+
+import lombok.Data;
+
+@Data
+public class NameIdClass {
+    private String classId;
+    private String className;
+}
---
+++ uniapp/back-end/query/query/src/main/java/com/scut/entities/NameIdUser.java
@@ -0,0 +1,9 @@
+package com.scut.entities;
+
+import lombok.Data;
+
+@Data
+public class NameIdUser {
+    private String id;
+    private String name;
+}
